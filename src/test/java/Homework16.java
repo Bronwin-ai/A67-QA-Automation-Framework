72494aaed8962393074d74434c2ade7022e46c5d
Bronwin-ai <emma.lghtnmlls@testpro.io>
2025-02-12 20:12:42 -0500

    public void RegistrationNaviation()

---
+++ src/test/java/Homework16.java
@@ -0,0 +1,37 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.Assert;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class Homework16 extends BaseTest{
+
+
+        @Test
+        public void RegistrationNaviation() {
+
+
+            ChromeOptions options = new ChromeOptions();
+            options.addArguments("--remote-allow-origins=*");
+
+            WebDriver driver = new ChromeDriver(options);
+            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+            String url = "https://qa.koel.app/";
+            driver.get(url);
+
+           WebElement regLink= driver.findElement(By.cssSelector("a[href*='registration']"));
+           regLink.click();
+
+           String webaddress="https://qa.koel.app/registration";
+
+
+            Assert.assertEquals(driver.getCurrentUrl(), webaddress);
+            driver.quit();
+        }
+    }
